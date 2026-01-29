183186e11b0e51401732f746f0a444d941fbaddb
bradykoenig <bkoenig12@icloud.com>
2026-01-28 23:09:27 -0600

    Converted to Cloudflare Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
 import Home from "./pages/Home";
